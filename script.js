eb8cd24d2ab2b817ccc19b0ea6b92ec6bb3ec8fd
Geremias <gereacosta1@gmail.com>
2024-07-17 00:31:00 -0400

    Add more javascript, fix something

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
-const display = document.querySelector("#display")
-const buttons = document.querySelectorAll("button")
+//Codigo en desuso por cosas viejas, que ya no se utilizan normalmente, como .subtr, eval, "Null"
 
-buttons.forEach((item)=>{
-    item.onclick=()=>{
-        if(item.id=="clear"){
-            display.innerText = "";
-        }else if (item.id=="backspace"){
-            let string = display.innerText.toString();
-            display.innerText = string.subtr(0, string.length - 1)
+// const display = document.querySelector("#display")
+// const buttons = document.querySelectorAll("button")
+
+// buttons.forEach((item)=>{
+//     item.onclick=()=>{
+//         if(item.id=="clear"){
+//             display.innerText = "";
+//         }else if (item.id=="backspace"){
+//             let string = display.innerText.toString();
+//             display.innerText = string.subtr(0, string.length - 1);
+//         } else if (display.innerText != "" && item.id == "eqyual"){
+//             display.innerText = eval(display.innerText);
+//         } else if (display.innerText = "Null"){
+//             setTimeout(() => (display.innerText = ""), 20000);
+
+//         }else {
+//             display.innerText +=item.id;
+//         }
+//     }
+// })
+
+
+
+
+
+
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+
+    buttons.forEach((button) => {
+    button.onclick = () => {
+        if (button.id === "clear") {
+        display.innerText = "";
+        } else if (button.id === "backspace") {
+        let string = display.innerText.toString();
+        display.innerText = string.slice(0, -1);
+        } else if (display.innerText !== "" && button.id === "equal") {
+        try {
+            display.innerText = eval(display.innerText);
+        } catch (e) {
+            display.innerText = "Error";
+            setTimeout(() => (display.innerText = ""), 2000);
         }
-    }
-})
+        } else if (display.innerText === "null") {
+        setTimeout(() => (display.innerText = ""), 2000);
+        } else {
+        display.innerText += button.id;
+        }
+    };
+    });

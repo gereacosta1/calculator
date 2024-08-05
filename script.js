15cc33b16a919ac7444e42dedac4bcb9ed122fac
Geremias <gereacosta1@gmail.com>
2024-08-05 03:59:55 -0400

    Improve UI and fix display issues

--- script.js
+++ script.js
@@ -26,27 +26,45 @@
 
 
 
-const display = document.querySelector("#display");
-const buttons = document.querySelectorAll("button");
+// const display = document.querySelector("#display");
+// const buttons = document.querySelectorAll("button");
 
-    buttons.forEach((button) => {
-    button.onclick = () => {
-        if (button.id === "clear") {
-        display.innerText = "";
-        } else if (button.id === "backspace") {
-        let string = display.innerText.toString();
-        display.innerText = string.slice(0, -1);
-        } else if (display.innerText !== "" && button.id === "equal") {
-        try {
-            display.innerText = eval(display.innerText);
-        } catch (e) {
-            display.innerText = "Error";
-            setTimeout(() => (display.innerText = ""), 2000);
-        }
-        } else if (display.innerText === "null") {
-        setTimeout(() => (display.innerText = ""), 2000);
-        } else {
-        display.innerText += button.id;
-        }
-    };
-    });
+//     buttons.forEach((button) => {
+//     button.onclick = () => {
+//         if (button.id === "clear") {
+//         display.innerText = "";
+//         } else if (button.id === "backspace") {
+//         let string = display.innerText.toString();
+//         display.innerText = string.slice(0, -1);
+//         } else if (display.innerText !== "" && button.id === "equal") {
+//         try {
+//             display.innerText = eval(display.innerText);
+//         } catch (e) {
+//             display.innerText = "Error";
+//             setTimeout(() => (display.innerText = ""), 2000);
+//         }
+//         } else if (display.innerText === "null") {
+//         setTimeout(() => (display.innerText = ""), 2000);
+//         } else {
+//         display.innerText += button.id;
+//         }
+//     };
+//     });
+
+    const screen = document.getElementById('screen');
+
+    function appendNumber(number) {
+    screen.innerText += number;
+    }
+
+    function clearScreen() {
+    screen.innerText = '';
+    }
+
+    function calculate() {
+    try {
+        screen.innerText = eval(screen.innerText);
+    } catch (error) {
+        screen.innerText = 'Error';
+    }
+    }
